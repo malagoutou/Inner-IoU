131d2d51b5f5b4dd74246853924ee6430991f45c
malagoutou <150572966+malagoutou@users.noreply.github.com>
2023-11-12 14:10:25 +0800

    Create innersiou.py

---
+++ innersiou.py
@@ -0,0 +1,41 @@
+def inner_siou(box1, box2, ratio = 0.8, xywh=True, eps=1e-7): 
+    (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, -1), box2.chunk(4, -1)
+    w1_, h1_, w2_, h2_ = w1 / 2, h1 / 2, w2 / 2, h2 / 2
+    b1_x1, b1_x2, b1_y1, b1_y2 = x1 - w1_, x1 + w1_, y1 - h1_, y1 + h1_
+    b2_x1, b2_x2, b2_y1, b2_y2 = x2 - w2_, x2 + w2_, y2 - h2_, y2 + h2_
+    #IoU       #IoU       #IoU       #IoU       #IoU       #IoU       #IoU       #IoU       #IoU       #IoU    
+    inter = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
+            (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
+    union = w1 * h1 + w2 * h2 - inter + eps
+
+   #Inner-IoU      #Inner-IoU        #Inner-IoU        #Inner-IoU        #Inner-IoU        #Inner-IoU        #Inner-IoU       
+
+    inner_b1_x1, inner_b1_x2, inner_b1_y1, inner_b1_y2 = x1 - w1_*ratio, x1 + w1_*ratio,\
+                                                             y1 - h1_*ratio, y1 + h1_*ratio
+    inner_b2_x1,inner_b2_x2, inner_b2_y1, inner_b2_y2 = x2 - w2_*ratio, x2 + w2_*ratio,\
+                                                             y2 - h2_*ratio, y2 + h2_*ratio
+    inner_inter = (torch.min(inner_b1_x2, inner_b2_x2) - torch.max(inner_b1_x1, inner_b2_x1)).clamp(0) * \
+                   (torch.min(inner_b1_y2, inner_b2_y2) - torch.max(inner_b1_y1, inner_b2_y1)).clamp(0)
+    inner_union = w1*ratio * h1*ratio + w2*ratio * h2*ratio - inner_inter + eps
+    inner_iou = inner_inter/inner_union
+
+    cw = torch.max(b1_x2, b2_x2) - torch.min(b1_x1, b2_x1)  # convex width
+    ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)  # convex height
+    s_cw = (b2_x1 + b2_x2 - b1_x1 - b1_x2) * 0.5 + eps
+    s_ch = (b2_y1 + b2_y2 - b1_y1 - b1_y2) * 0.5 + eps
+    sigma = torch.pow(s_cw ** 2 + s_ch ** 2, 0.5)
+    sin_alpha_1 = torch.abs(s_cw) / sigma
+    sin_alpha_2 = torch.abs(s_ch) / sigma
+    threshold = pow(2, 0.5) / 2
+    sin_alpha = torch.where(sin_alpha_1 > threshold, sin_alpha_2, sin_alpha_1)
+    angle_cost = torch.cos(torch.arcsin(sin_alpha) * 2 - math.pi / 2)
+    rho_x = (s_cw / cw) ** 2
+    rho_y = (s_ch / ch) ** 2
+    gamma = angle_cost - 2
+    distance_cost = 2 - torch.exp(gamma * rho_x) - torch.exp(gamma * rho_y)
+    omiga_w = torch.abs(w1 - w2) / torch.max(w1, w2)
+    omiga_h = torch.abs(h1 - h2) / torch.max(h1, h2)
+    shape_cost = torch.pow(1 - torch.exp(-1 * omiga_w), 4) + torch.pow(1 - torch.exp(-1 * omiga_h), 4)
+    inner_siou = inner_iou - 0.5 * (distance_cost + shape_cost)
+
+    return inner_siou  # IoU
